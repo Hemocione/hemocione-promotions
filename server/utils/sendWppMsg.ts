c8a7fc8e7ab8aecb09d734cc52807a877d242cac
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-20 19:12:43 -0300

    add hemocioneID

--- server/utils/sendWppMsg.ts
+++ server/utils/sendWppMsg.ts
@@ -1,5 +1,5 @@
 // needs to define endpoint for wpp at hemocione-id
-export async function sendWppMsg(username: string, userEmail: string, coupon: string) {
+export async function sendWppMsg(username: string, userEmail: string, hemocioneID: string, coupon: string) {
   const data = await $fetch(`${process.env.hemocioneIdApiUrl}/wpp`, {
     method: 'POST',
     headers: {
@@ -8,9 +8,11 @@ export async function sendWppMsg(username: string, userEmail: string, coupon: st
     body: {
       coupon,
       username,
-      userEmail
+      userEmail,
+      hemocioneID
     }
   })
 
+  // the data returned could be 'Message received', with status 200.
   return data
 }
