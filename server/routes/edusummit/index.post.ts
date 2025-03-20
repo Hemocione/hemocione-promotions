1da8b75b98102ea29ce4dff22ed0fbdea66059b2
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-20 19:12:53 -0300

    define post

--- server/routes/edusummit/index.post.ts
+++ server/routes/edusummit/index.post.ts
@@ -1,3 +1,22 @@
-export default defineEventHandler((event) => {
-  return "Start by editing <code>server/routes/index.ts</code>.";
+import { H3Event } from 'h3'
+import { getCoupon } from '~~/promotion/edusummit/jobs/getCoupon';
+
+export default defineEventHandler(async (event: H3Event) => {
+  const body = await readBody(event)
+  const coupon = getCoupon()
+
+  try {
+    sendWppMsg(body.username, body.userEmail, body.hemocioneID, coupon)
+    // registerMetric()
+    setResponseStatus(event, 200)
+    return {
+      message: 'Message received'
+    }
+  } catch (e) {
+    setResponseStatus(event, 500)
+    // registerMetric()
+    return {
+      message: 'Error sending message'
+    }
+  }
 });
