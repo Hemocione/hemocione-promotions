40797818e22d31f28f8c698e7673ce08ed5e112d
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-20 19:12:05 -0300

    remove useless files

--- promotion/edusummit/index.ts
+++
@@ -1,9 +0,0 @@
-import { getCoupon } from "./jobs/getCoupon";
-import { sendWppMsg } from "./jobs/sendWppMsg";
-
-export async function main(req) {
-  const coupon = getCoupon();
-  const { username, userEmail } = req.body;
-  const data = await sendWppMsg(username, userEmail, coupon);
-  return { status: data.status, message: data.message };
-}
--- server/routes/edusummit/index.get.ts
+++
@@ -1,3 +0,0 @@
-export default defineEventHandler((event) => {
-  return "Start by editing aqui.";
-});
