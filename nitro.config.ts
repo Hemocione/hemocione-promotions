89973789c3b69268556773cbb728c958c16f00c9
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-20 16:48:16 -0300

    Add runtrimeconfig

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,5 +3,10 @@ export default defineNitroConfig({
   srcDir: "server",
   compatibilityDate: "2025-03-20",
   runtimeConfig: {
+    public: {
+      hemocioneIdApiUrl:
+        process.env.HEMOCIONE_ID_API_URL ??
+        "https://hemocione-id-dev.cpt.hemocione.com.br",
+    }
   }
 });
