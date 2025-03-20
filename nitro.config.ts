7eb7f602de57832d209592121b4d36c373856016
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-20 12:16:47 -0300

    Add runtimeconfig

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,7 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2025-03-20"
+  compatibilityDate: "2025-03-20",
+  runtimeConfig: {
+  }
 });
