2b99d010349622e99e2cfb2e13e2177c804849bf
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-21 12:44:23 -0300

    Add vercel waitUntil + add createError

--- server/api/v1/[promotionSlug]/trigger-async.post.ts
+++ server/api/v1/[promotionSlug]/trigger-async.post.ts
@@ -1,16 +1,17 @@
 import { H3Event } from "h3";
 import { getPromotion } from "~/services/promotions/service";
+import { waitUntil } from '@vercel/functions';
+import { runWithinCall } from "~/services/promotions-call/service";
 
 export default defineEventHandler(async (event: H3Event) => {
   const body = await readBody(event);
   const promotionSlug = getRouterParam(event, "promotionSlug");
   const promotion = getPromotion(promotionSlug);
   if (!promotion) {
-    // throw not found
-    throw new Error("Promotion not found");
+    throw createError({ statusCode: 404, statusMessage: "Promotion not found" });
   }
-  // waitUntil da vercel - https://vercel.com/docs/functions/functions-api-reference/vercel-functions-package#waituntil
-  // waitUntil(runWithinCall(promotion.webhookHandler), body);
+
+  waitUntil(runWithinCall(promotion.webhookHandler, body));
 
   return {
     status: 200,
