09aaf1150649cadf66a3c5dec2bf34a168b43342
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-21 12:43:02 -0300

    Mark how it must be done when endpoint is ready

--- server/services/hemocione-id/service.ts
+++ server/services/hemocione-id/service.ts
@@ -1,3 +1,11 @@
 export async function sendWppMsg(payload: any) {
-  console.log("sendWppMsg");
+  console.log("calls hemocione-id to send wpp msg");
+  // Example:
+  // $fetch("https://hemocione-id.com/send-wpp-msg", {
+  //   method: "POST",
+  //   headers: {
+  //      'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify(payload),
+  // });
 }
