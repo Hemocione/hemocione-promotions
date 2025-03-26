566c33524331dca7a6c01552b377de792310f147
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-03-25 23:10:26 -0300

    Add promotions request

--- server/services/hemocione-id/service.ts
+++ server/services/hemocione-id/service.ts
@@ -1,11 +1,9 @@
 export async function sendWppMsg(payload: any) {
-  console.log("calls hemocione-id to send wpp msg");
-  // Example:
-  // $fetch("https://hemocione-id.com/send-wpp-msg", {
-  //   method: "POST",
-  //   headers: {
-  //      'Content-Type': 'application/json'
-  //   },
-  //   body: JSON.stringify(payload),
-  // });
+  $fetch("https://hemocione-id.com/send-wpp-msg", {
+    method: "POST",
+    headers: {
+       'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload),
+  });
 }
