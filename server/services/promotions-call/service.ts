aff06e8e5a5d8173c8c4f48c66a721dc1a4f1c6d
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-30 20:25:46 -0300

    Add todo + comment call.save

--- server/services/promotions-call/service.ts
+++ server/services/promotions-call/service.ts
@@ -2,6 +2,8 @@ export const runWithinCall = async (
   fn: (payload: unknown) => Promise<void>,
   payload: unknown
 ) => {
+  //  call will be registered at any db as an object
+  // with info based on the call, like startedAt, endedAt, status, error
   const call: any = { startedAt: new Date(), payload }; // create the call
   try {
     await fn(payload);
@@ -11,5 +13,5 @@ export const runWithinCall = async (
     call.error = e;
   }
   call.endedAt = new Date();
-  call?.save();
+  // call?.save();
 };
