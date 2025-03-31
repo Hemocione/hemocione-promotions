159268bd54a388301353728ce5674bc2d2b4a2bb
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-31 00:10:08 -0300

    add zoid typing, template name and template components

--- server/services/promotions/edusummit/index.ts
+++ server/services/promotions/edusummit/index.ts
@@ -3,17 +3,53 @@ import { Payload } from "./interface";
 import { z } from "zod"
 
 const payloadSchema = z.object({
-  name: z.string(),
-  email: z.string(),
+  templateComponents: z.array(
+    z.object({
+      type: z.union([z.literal("body"), z.literal("button")]),
+      sub_type: z.literal("url").optional(), // Only present for buttons
+      index: z.string().optional(), // Only present for buttons
+      parameters: z.array(z.object({
+        type: z.literal("text"),
+        parameter_name: z.string().optional(), // Only present in some objects
+        text: z.string()
+      }))
+    })
+  ),
+  templateName: z.string(),
   hemocioneId: z.string(),
 })
 
 export const webhookHandler = async (payload: Payload) => {
-  // get code from env, maybe?
   const coupon = 'fixedCode'
+
+  const templateName = 'edusummit_cupom_2025'
+
+  const templateComponents = [
+    {
+      "type": "body",
+      "parameters": [
+        {
+          "type": "text",
+          "parameter_name": "coupon",
+          "text": "Pi"
+        }
+      ]
+    },
+    {
+      "type": "button",
+      "sub_type": "url",
+      "index": "0",
+      "parameters": [
+        {
+          "type": "text",
+          "text": "https://hemocione.com.br/"
+        }
+      ]
+    }
+  ]
 
   // validate payload
   payloadSchema.parse(payload)
 
-  sendWppMsg({ ...payload, coupon });
+  sendWppMsg({ templateName, templateComponents, coupon });
 };
