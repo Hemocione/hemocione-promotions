02cba2f496477b8e782028784fa6081e1eee2fe5
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-04-01 00:45:33 -0300

    Create middleware

---
+++ server/middleware/1.authenticate.ts
@@ -0,0 +1,22 @@
+import { defineEventHandler, createError, getRequestHeader } from 'h3'
+
+export default defineEventHandler(async (event) => {
+  try {
+    const config = useRuntimeConfig()
+    const secret = getRequestHeader(event, 'x-secret')
+
+    if (!secret || secret !== config.secret) {
+      return sendError(event, createError({
+        statusCode: 401,
+        statusMessage: 'Credenciais inválidas.'
+      }))
+    }
+
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Erro interno do servidor.',
+      cause: error
+    })
+  }
+})
