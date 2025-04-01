f7340390b38f2d9d79a1f0fcd25821997c98b1ac
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2025-04-01 10:04:47 -0300

    Add Vercel preset and new async trigger endpoint for promotions

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,16 +2,16 @@
 export default defineNitroConfig({
   srcDir: "server",
   compatibilityDate: "2025-03-20",
+  preset: "vercel",
   runtimeConfig: {
     public: {
       hemocioneIdApiUrl:
-        process.env.HEMOCIONE_ID_API_URL ??
-        "http://localhost:8080",
+        process.env.HEMOCIONE_ID_API_URL ?? "http://localhost:8080",
     },
     secret: process.env.API_SECRET ?? "secret",
     eduSummit: {
       coupon: process.env.EDUSUMMIT_COUPON_CODE ?? "T3ST3",
       url: process.env.EDUSUMMIT_URL ?? "https://www.example.com",
     },
-  }
+  },
 });
