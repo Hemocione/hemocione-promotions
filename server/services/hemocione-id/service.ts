6e1cb2bbd5b1428ef06205a66f5f5ed4ab4acf6d
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2025-05-20 15:46:02 -0300

    Enhance sendWppMsg function with logging for payload and streamline body assignment

--- server/services/hemocione-id/service.ts
+++ server/services/hemocione-id/service.ts
@@ -1,11 +1,13 @@
 export async function sendWppMsg(payload: any) {
   const config = useRuntimeConfig();
+  const body = JSON.stringify(payload);
+  console.log("Sending WPP message to Hemocione ID:", body);
   await $fetch(`${config.public.hemocioneIdApiUrl}/send-wpp-msg`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "x-secret": config.hemocioneIdApiSecret,
     },
-    body: JSON.stringify(payload),
+    body,
   });
 }
